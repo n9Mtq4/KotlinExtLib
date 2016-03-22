9d2317730d62289f5d250881a17f3360d539c5fd
n9Mtq4 <bill.wresnahan@gmail.com>
2016-03-21 22:32:46 -0400

    syntax candy and System.err.print

---
+++ src/main/kotlin/com/n9mtq4/kotlin/extlib/io/Console.kt
@@ -0,0 +1,14 @@
+package com.n9mtq4.kotlin.extlib.io
+
+/**
+ * Created by will on 3/21/16 at 9:03 PM.
+ *
+ * @author Will "n9Mtq4" Bresnahan
+ */
+fun errPrint(a: Any) {
+	System.err.print(a)
+}
+
+fun errPrintln(a: Any) {
+	System.err.println(a)
+}
---
+++ src/main/kotlin/com/n9mtq4/kotlin/extlib/syntax/Functions.kt
@@ -0,0 +1,8 @@
+package com.n9mtq4.kotlin.extlib.syntax
+
+/**
+ * Created by will on 3/21/16 at 9:11 AM.
+ *
+ * @author Will "n9Mtq4" Bresnahan
+ */
+inline fun <T> f(body: () -> T) = body.invoke()
