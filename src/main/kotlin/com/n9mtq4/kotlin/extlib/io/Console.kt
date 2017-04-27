b99571e30933ee359a41c4236f362e8033b19ed1
n9Mtq4 <bill.wresnahan@gmail.com>
2017-04-26 22:58:42 -0400

    errPrints for each primitive

--- src/main/kotlin/com/n9mtq4/kotlin/extlib/io/Console.kt
+++ src/main/kotlin/com/n9mtq4/kotlin/extlib/io/Console.kt
@@ -12,3 +12,75 @@ fun errPrint(a: Any) {
 fun errPrintln(a: Any) {
 	System.err.println(a)
 }
+
+fun errPrint(s: String) {
+	System.err.print(s)
+}
+
+fun errPrintln(s: String) {
+	System.err.println(s)
+}
+
+fun errPrint(b: Byte) {
+	System.err.print(b)
+}
+
+fun errPrintln(b: Byte) {
+	System.err.println(b)
+}
+
+fun errPrint(s: Short) {
+	System.err.print(s)
+}
+
+fun errPrintln(s: Short) {
+	System.err.println(s)
+}
+
+fun errPrint(i: Int) {
+	System.err.print(i)
+}
+
+fun errPrintln(i: Int) {
+	System.err.println(i)
+}
+
+fun errPrint(l: Long) {
+	System.err.print(l)
+}
+
+fun errPrintln(l: Long) {
+	System.err.println(l)
+}
+
+fun errPrint(f: Float) {
+	System.err.print(f)
+}
+
+fun errPrintln(f: Float) {
+	System.err.println(f)
+}
+
+fun errPrint(d: Double) {
+	System.err.print(d)
+}
+
+fun errPrintln(d: Double) {
+	System.err.println(d)
+}
+
+fun errPrint(b: Boolean) {
+	System.err.print(b)
+}
+
+fun errPrintln(b: Boolean) {
+	System.err.println(b)
+}
+
+fun errPrint(c: Char) {
+	System.err.print(c)
+}
+
+fun errPrintln(c: Char) {
+	System.err.println(c)
+}
